05982d88fd70eca6b90974ce449e28fb12bf4525
WillianyV <41123970+WillianyV@users.noreply.github.com>
2022-02-17 11:20:40 -0300

    Back-end de função part 2
    Controller;
    Store request
    Update Request;

---
+++ app/Http/Requests/UpdateRoleRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRoleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'description' => "bail|required|min:3|max:100|unique:roles,description,{$this->role->id}",
+            'status'      => 'bail|required|boolean',
+        ];
+    }
+}
