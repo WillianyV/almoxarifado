e40058405ef7c1a9ab0e3398f0c6413348334b4b
WillianyV <41123970+WillianyV@users.noreply.github.com>
2022-02-18 10:51:48 -0300

    Corrigindo o problema do modal delete

--- resources/views/setor/modal/delete.blade.php
+++ resources/views/setor/modal/delete.blade.php
@@ -1,22 +1,21 @@
 <form method="POST" action="{{ route('setor.destroy', $department->id) }}">
     @method('DELETE')
-    @csrf
-    <div class="modal fade" id="modalDeletar{{$department->id}}" tabindex="-1"
-        aria-labelledby="modalDeletarLabel{{$department->id}}" aria-hidden="true">
+    @csrf                                            
+    <div class="modal fade" id="deleteModal{{ $department->id }}" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modalDeletarLabel{{$department->id}}">Deletar</h5>
+                    <h5 class="modal-title" id="deleteModalLabel">Exclusão</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    Certeza que deseja exluir?
+                    Deseja excluir o setor: {{ $department->description }}?
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                    <button type="button" class="btn btn-primary">Salvar</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-danger">Excluir</button>
                 </div>
             </div>
         </div>
     </div>
-</form>
+</form> 
